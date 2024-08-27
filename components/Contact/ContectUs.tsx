b6738ed610f37eddfaa2a65118996a7e658e5904
AbdulSami969 <58950012+AbdulSami969@users.noreply.github.com>
2024-08-28 00:42:21 +0500

    finalized form

--- components/Contact/ContectUs.tsx
+++ components/Contact/ContectUs.tsx
@@ -48,10 +48,10 @@ const ContectUs = () => {
       <form onSubmit={handleSubmit} className="p-5 py-10 sm:p-24  text-black ">
         <input type="text" placeholder="Full Name" name="name" value={formData.name} onChange={handleInputChange} className="w-full font-extralight px-3 py-4 text-base border border-[#e8eae8] rounded-lg  hover:border-[#017e48] focus:border-2 focus:border-[#017e48]" required />
         <div className=" mt-5">
-          <input type="email" placeholder="Email address" name="email" value={formData.email} onChange={handleInputChange} className="w-full font-extralight px-3 py-4 text-sm border border-[#e8eae8] rounded-lg  hover:border-[#017e48] focus:border-2 focus:border-[#017e48]" required />
+          <input type="email" placeholder="Email address" name="email" value={formData.email} onChange={handleInputChange} className="w-full font-extralight px-3 py-4 text-base border border-[#e8eae8] rounded-lg  hover:border-[#017e48] focus:border-2 focus:border-[#017e48]" required />
         </div>
         <div className="mt-5">
-          <input type="city" placeholder="City" name="city" value={formData.city} onChange={handleInputChange} className="w-full font-extralight px-3 py-4 text-sm border border-[#e8eae8] rounded-lg  hover:border-[#017e48] focus:border-2 focus:border-[#017e48]" required />
+          <input type="city" placeholder="City" name="city" value={formData.city} onChange={handleInputChange} className="w-full font-extralight px-3 py-4 text-base border border-[#e8eae8] rounded-lg  hover:border-[#017e48] focus:border-2 focus:border-[#017e48]" required />
         </div>
 
         <div></div>
