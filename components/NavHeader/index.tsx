8f2265e308fac1ae3c86f1417da7c30264b76a5e
AbdulSami969 <58950012+AbdulSami969@users.noreply.github.com>
2024-09-28 15:15:01 +0500

    Multilanguage button design updated as per provided design

--- components/NavHeader/index.tsx
+++ components/NavHeader/index.tsx
@@ -5,7 +5,7 @@ import { FiAlignJustify } from "react-icons/fi";
 import { urlFor } from "@/client";
 import Link from "next/link";
 import { usePathname, useRouter } from "next/navigation";
-import { FiChevronDown } from "react-icons/fi"; // Icon for dropdown arrow
+import { FiChevronDown, FiGlobe } from "react-icons/fi"; // Icon for dropdown arrow
 
 const NavHeader = ({ data }: { data: any }) => {
   const [showOptions, setShowOptions] = useState(false);
@@ -21,10 +21,14 @@ const NavHeader = ({ data }: { data: any }) => {
   const changeLanguage = (newLocale: string) => {
     setSelectedLocale(newLocale);
     setDropdownOpen(false);
+
+    // Replace the current locale with the new locale in the pathname
+    const newPathname = pathname.replace(`/${locale}`, `/${newLocale}`);
+
     // Navigate to the same path but with the new locale
-    router.push(`/${newLocale}`);
+    router.push(newPathname);
   };
-  console.log("pathname: " + pathname);
+
   return (
     <header className="absolute left-0 right-0 flex flex-col bg-[#f9f6f1] md:bg-transparent md:flex-row justify-between items-center px-5 border-b border-[#2325231a] z-50">
       <div className="flex justify-between w-full md:w-auto items-center h-20">
@@ -38,24 +42,24 @@ const NavHeader = ({ data }: { data: any }) => {
 
       <nav className={`flex flex-col md:flex-row gap-2 md:gap-5 pb-5 md:pb-0 items-center w-full md:w-auto ${showOptions ? "block" : "hidden"} md:flex`}>
         {/* Custom dropdown */}
-        {(pathname == "/it" || pathname == "/en") && (
-          <div className="relative">
-            <div className="flex items-center justify-between  px-4 py-2 rounded-md cursor-pointer hover:border-gray-400 focus:outline-none" onClick={toggleDropdown}>
-              <span className="text-black text-base">{selectedLocale === "en" ? "English" : "Italian"}</span>
-              <FiChevronDown className="ml-2 text-black" />
-            </div>
-            {isDropdownOpen && (
-              <div className="absolute mt-[-2px] bg-[#faf7f2] border border-gray-200 rounded-md shadow-lg w-full z-10">
-                <div className={`px-4 py-2 text-black text-base cursor-pointer hover:bg-[#ede9e6] transition-colors ${selectedLocale === "en" ? "font-bold" : ""}`} onClick={() => changeLanguage("en")}>
-                  English
-                </div>
-                <div className={`px-4 py-2 text-black text-base cursor-pointer hover:bg-[#ede9e6] transition-colors ${selectedLocale === "it" ? "font-bold" : ""}`} onClick={() => changeLanguage("it")}>
-                  Italian
-                </div>
-              </div>
-            )}
+        <div className="relative">
+          <div className="flex items-center justify-between px-4 py-2 rounded-md cursor-pointer hover:border-gray-400 focus:outline-none" onClick={toggleDropdown}>
+            {/* Globe Icon */}
+            <FiGlobe className="mr-2 text-black" />
+            <span className="text-black text-base">{selectedLocale === "en" ? "EN" : "IT"}</span>
+            <FiChevronDown className="ml-2 text-black" />
           </div>
-        )}
+          {isDropdownOpen && (
+            <div className="absolute mt-[-2px] bg-[#faf7f2] border border-gray-200 rounded-md shadow-lg z-10">
+              <div className={`px-2 mx-2 py-1 my-1 text-black text-sm cursor-pointer hover:bg-[#ede9e6] transition-colors ${selectedLocale === "en" ? "font-bold" : ""}`} onClick={() => changeLanguage("en")}>
+                English
+              </div>
+              <div className={`px-2 mx-2 py-1 my-1 text-black text-sm cursor-pointer hover:bg-[#ede9e6] transition-colors ${selectedLocale === "it" ? "font-bold" : ""}`} onClick={() => changeLanguage("it")}>
+                Italian
+              </div>
+            </div>
+          )}
+        </div>
 
         {/* Other links */}
         <Link href={pathname === "/blogs" ? "/blogs" : `/${locale}/blogs`} className="text-[#232523] font-bold text-lg py-5">
