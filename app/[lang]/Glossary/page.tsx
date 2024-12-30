73d47e61b97cc413868dafd7a908199b26e35fbf
AbdulSami969 <58950012+AbdulSami969@users.noreply.github.com>
2024-12-30 12:12:40 +0500

    google tag updated

--- app/[lang]/Glossary/page.tsx
+++ app/[lang]/Glossary/page.tsx
@@ -3,6 +3,7 @@ import Card from "@/components/Glossary/Card/Card";
 import Hero from "@/components/Glossary/Hero/Hero";
 import Footer from "@/components/Footer";
 import { getGlossaryPage, getHeaderFooter } from "@/utils/query";
+import GoogleAnalyticsWrapper from "@/components/GoogleAnalyticsWrapper";
 // import { getGlossary } from "@/utils/query";
 const Page = async ({ params, searchParams }: { params: any; searchParams: any }) => {
   const glossary = searchParams.glossary ? searchParams.glossary : "";
@@ -10,13 +11,16 @@ const Page = async ({ params, searchParams }: { params: any; searchParams: any }
   const glossaryPage = await getGlossaryPage(params.lang);
   console.log("glossaryPage", glossaryPage);
   return (
-    <div>
-      <Hero header={navs?.header} heading={glossaryPage} />
-      <Card lang={params.lang} glossary={glossary} />
-      <div className="mt-8 sm:mt-20">
-        <Footer footer={navs?.header} />
+    <>
+      {process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID && <GoogleAnalyticsWrapper GA_MEASUREMENT_ID={process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID} />}
+      <div>
+        <Hero header={navs?.header} heading={glossaryPage} />
+        <Card lang={params.lang} glossary={glossary} />
+        <div className="mt-8 sm:mt-20">
+          <Footer footer={navs?.header} />
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- app/[lang]/game-detail/[game]/page.tsx
+++ app/[lang]/game-detail/[game]/page.tsx
@@ -3,15 +3,19 @@ import Card from "@/components/Card/CardPage/Home";
 // import Header from '@/components/Card/CardPage/Header'
 import PrizeCard from "@/components/Card/CardPage/PrizeCard";
 import { getGamesDetail, getHeaderFooter } from "@/utils/query";
+import GoogleAnalyticsWrapper from "@/components/GoogleAnalyticsWrapper";
 
 const page = async ({ params }: { params: any }) => {
   const data = await getGamesDetail(params.game);
   const navs = await getHeaderFooter();
 
   return (
-    <div>
-      <Card lang={params.lang} header={navs?.header} data={data?.games} />
-    </div>
+    <>
+      {process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID && <GoogleAnalyticsWrapper GA_MEASUREMENT_ID={process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID} />}
+      <div>
+        <Card lang={params.lang} header={navs?.header} data={data?.games} />
+      </div>
+    </>
   );
 };
 
